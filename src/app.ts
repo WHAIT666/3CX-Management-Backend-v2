04577aceee8cd29a62fe545b480aff8497f2917c
André Santos <117940191+WHAIT666@users.noreply.github.com>
2024-06-11 17:49:24 +0100

    chore: Refactor createUserHandler to use updated dependencies and send password reset email

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ app.use(express.json());
 
 app.use(router);
 
-
 const port = config.get("port");
 
 app.listen(port, () => {
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,32 +1,31 @@
-import {Request, Response } from 'express';
-import { CreateUserInput } from '../schema/user.schema';
-import { createUser } from '../service/user.service'
-import sendEmail from '../utils/mailer';
+import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+import { CreateUserInput } from "../schema/user.schema";
+import { createUser } from "../service/user.service";
+import  sendEmail  from "../utils/mailer";
 
 export async function createUserHandler(
-    req: Request<{}, {}, CreateUserInput>,
-    res: Response){
+  req: Request<{}, {}, CreateUserInput>,
+  res: Response
+) {
+  const body = req.body;
 
-    const body = req.body;
-    
-    try{
-        const user = await createUser(body);
+  try {
+    const user = await createUser(body);
 
-        await sendEmail({
-            to: user.email,
-            from: "test@example.com",
-            subject: "Reset your password",
-            text: `Password reset code: ${user.verificationCode}. Id ${user._id}`,
-          });
-    
-    return res.send("User sucessfuly Created")
-    }catch(e){
-        if(e.code === 11000){
-            return res.status(409).send("User already exists")
-        }
-    
+    await sendEmail({
+      to: user.email,
+      from: "test@example.com",
+      subject: "Verify your email",
+      text: `Verification code: ${user.verificationCode}. User ID: ${user._id}`,
+    });
 
-        return res.status(500).send(e);
+    return res.send("User successfully created");
+  } catch (e: any) {
+    if (e.code === 11000) {
+      return res.status(409).send("Account already exists");
     }
 
+    return res.status(500).send(e);
+  }
 }
