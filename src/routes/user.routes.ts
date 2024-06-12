b84c67105d1d00abd0a4e7c0616f698fb81bed70
André Santos <117940191+WHAIT666@users.noreply.github.com>
2024-06-12 13:38:40 +0100

    chore: Refactor createUserHandler to use updated dependencies and send password reset email

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import {
   createUserHandler,
-
+  forgotPasswordHandler,
+  resetPasswordHandler,
+  verifyUserHandler,
 } from "../controller/user.controller";
 import validateResource from "../middleware/validateResource";
 import {
   createUserSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+  verifyUserSchema,
 } from "../schema/user.schema";
 
 const router = express.Router();
@@ -14,6 +19,24 @@ router.post(
   "/api/users",
   validateResource(createUserSchema),
   createUserHandler
+);
+
+router.post(
+  "/api/users/verify/:id/:verificationCode",
+  validateResource(verifyUserSchema),
+  verifyUserHandler
+);
+
+router.post(
+  "/api/users/forgotpassword",
+  validateResource(forgotPasswordSchema),
+  forgotPasswordHandler
+);
+
+router.post(
+  "/api/users/resetpassword/:id/:passwordResetCode",
+  validateResource(resetPasswordSchema),
+  resetPasswordHandler
 );
 
 
