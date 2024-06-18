4e77ef96f15a78586c589b593f847e87b6db9ebb
André Santos <117940191+WHAIT666@users.noreply.github.com>
2024-06-18 15:00:06 +0100

    chore: Update dependencies and add CORS support
    This commit updates the dependencies in the project, specifically adding the "cors" package to enable Cross-Origin Resource Sharing (CORS) support. CORS allows the frontend application to make requests to the backend API from a different domain. This change is made to improve the overall functionality and security of the codebase.
    
    Fixed cors issue by installing dependency.
    User logged in name now displaying correctly

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-require("dotenv").config();
-import express from "express";
-import config from "config";
-import connectToDb from "./utils/connectToDb";
-import log from "./utils/logger";
-import router from "./routes";
-import deserializeUser from "./middleware/deserializeUser";
+require('dotenv').config();
+import express from 'express';
+import config from 'config';
+import cors from 'cors';
+import connectToDb from './utils/connectToDb';
+import log from './utils/logger';
+import router from './routes';
+import deserializeUser from './middleware/deserializeUser';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Update this with your frontend URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
@@ -14,10 +26,9 @@ app.use(deserializeUser);
 
 app.use(router);
 
-const port = config.get("port");
+const port = config.get('port');
 
 app.listen(port, () => {
   log.info(`App started at http://localhost:${port}`);
-
   connectToDb();
 });
